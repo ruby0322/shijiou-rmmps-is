72e7740aabe0f1777be5cc77f075f330acbfad4a
Ruby <ruby0322@ntu.im>
2023-05-20 23:38:36 +0800

    Remove Analytics Service from Backend

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -21,7 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db, analytics };
