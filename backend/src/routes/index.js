9aada1f54fdb0d77280723f5c0c5a222922a73d4
mchen127 <mchen127.p@gmail.com>
2023-05-23 21:36:15 +0800

    add todayspecial router to main

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -10,6 +10,7 @@ const router = express.Router();
 import testRouter from './test.js';
 import botRouter from './bot.js';
 import menuRouter from './menu.js';
+import todaySpecialRouter from './todayspecial.js';
 
 router.get("/", (req, res) => {
     res.status(200).send('Hello from RMMPS.');
@@ -19,6 +20,7 @@ router.use('/bot', botRouter);
 router.use(express.json());
 router.use('/test', testRouter);
 router.use("/menu", menuRouter);
+router.use("/todayspecial", todaySpecialRouter);
 
 // *** End of routers ***
 
