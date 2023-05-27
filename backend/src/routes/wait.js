68de1c2f56fafe4b5e380a56c1d81f097af1143e
mchen127 <mchen127.p@gmail.com>
2023-05-27 20:52:57 +0800

    wait api v1

--- backend/src/routes/wait.js
+++ backend/src/routes/wait.js
@@ -11,4 +11,128 @@ import {
 } from "firebase/firestore";
 
 const waitRequestRouter = express.Router();
-const menuRef = collection(db, "menuItems");
+const todayWaitCollection = "todayWaitRef";
+const historyWaitCollection = "historyWaitRef";
+const todayWaitRef = collection(db, todayWaitCollection);
+const historyWaitRef = collection(db, historyWaitCollection);
+
+
+waitRequestRouter.get("/getTodayWait", async (req, res) => {
+    try {
+        const todayWaitRequests = await getDocs(todayWaitRef);
+        let todayWaitList = [];
+        todayWaitRequests.forEach((doc) => {
+            todayWaitList.push({ waitReqId: doc.id, ...doc.data() });
+            console.log(doc.id, " => ", doc.data());
+        });
+        res.status(200).json({
+            todayWaitList,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+
+waitRequestRouter.put("/notify", async (req, res) => {
+    try {
+        const waitReqId = req.body.waitReqId;
+        const waitReqRef = doc(db, todayWaitCollection, waitReqId);
+        await updateDoc(waitReqRef, {
+            status: "notified",
+        });
+        res.status(200).json({
+            message: "wait request status is updated to notified",
+            waitReqId: waitReqId,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+waitRequestRouter.put("/done", async (req, res) => {
+    try {
+        const waitReqId = req.body.waitReqId;
+        const waitReqRef = doc(db, todayWaitCollection, waitReqId);
+        await updateDoc(waitReqRef, {
+            isWaiting: false,
+            status: "arrived",
+        });
+        res.status(200).json({
+            message: "wait request status is updated to arrived",
+            waitReqId: waitReqId,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+waitRequestRouter.put("/remove", async (req, res) => {
+    try {
+        const waitReqId = req.body.waitReqId;
+        const waitReqRef = doc(db, todayWaitCollection, waitReqId);
+
+        await updateDoc(waitReqRef, {
+            isWaiting: false,
+            status: "removed",
+        });
+
+        res.status(200).json({
+            message: "wait request status is updated to removed",
+            waitReqId: waitReqId,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+waitRequestRouter.put("/clear", async (req, res) => {
+    try {
+        const todayWaitRequests = await getDocs(todayWaitRef);
+        todayWaitRequests.forEach(async (doc) => {
+            const docData = { ...doc.data() };
+            if (docData.isWaiting === false) {
+                await addDoc(historyWaitRef, { ...docData });
+                const docRef = doc(db, todayWaitCollection, doc.id);
+                await deleteDoc(docRef);
+            }
+        });
+        res.status(200).json({
+            message: "wait request is moved to history",
+            waitReqId: waitReqId,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+waitRequestRouter.put("/removeAll", async (req, res) => {
+    try {
+        const todayWaitRequests = await getDocs(todayWaitRef);
+        todayWaitRequests.forEach(async (doc) => {
+            const docRef = doc(db, todayWaitCollection, doc.id);
+            const docData = { ...doc.data() };
+            if (docData.isWaiting === true) {
+                await addDoc(historyWaitRef, {
+                    isWaiting: false,
+                    status: "removed",
+                    ...docData,
+                });
+            } else {
+                await addDoc(historyWaitRef, {
+                    ...docData,
+                });
+            }
+            await deleteDoc(docRef);
+        });
+        res.status(200).json({
+            message: "wait request status is updated to removed",
+            waitReqId: waitReqId,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+export default waitRequestRouter;
--- backend/src/schema/WaitRequest.js
+++ backend/src/schema/WaitRequest.js
@@ -1,7 +1,8 @@
 const WaitRequest = class {
-    constructor(waitingNumber, lineUserId, status, groupSize, requestMadeTime, requestAnsweredTime, arriveTime, cancelTime) {
+    constructor(waitingNumber, lineUserId, isWaiting, status, groupSize, requestMadeTime, requestAnsweredTime, arriveTime, cancelTime) {
         this.waitingNumber = waitingNumber;
         this.lineUserId = lineUserId;
+        this.isWaiting = isWaiting;
         this.status = status;
         this.groupSize = groupSize;
         this.requestMadeTime = requestMadeTime;
