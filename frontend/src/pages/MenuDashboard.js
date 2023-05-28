db0e4334630d84427fb9b95314fc3b2644de8a99
Min Lai <mimi.lai.cat@gmail.com>
2023-05-28 16:35:27 +0800

    MenuDashboard add Add and fix bugs

--- frontend/src/pages/MenuDashboard.js
+++ frontend/src/pages/MenuDashboard.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import instance from "../axios";
 import fakeMenu from "../components/fakeMenu";
 import { Tabs, Card, Modal, Input, InputNumber, Button, Radio } from 'antd';
-import { EditOutlined } from '@ant-design/icons';
+import { EditOutlined, PlusCircleOutlined } from '@ant-design/icons';
 
 const Menu = () => {
   const [menuList, setMenuList] = useState({});
   const [editModal, setEditModal] = useState(""); //存 item 物件
+  const [addModal, setAddModal] = useState(false); // true/false
 
   useEffect(() => {
     fetchMenu();
@@ -93,32 +94,47 @@ const Menu = () => {
               }).map((item) => {
                 return (
                   item.status === 'serving' ? 
-                  <Card size="small" onClick={() => console.log(item.itemName)} style={{ width: '150px', backgroundColor: 'lightgreen', fontSize: "16px", color: 'black' }} 
+                  <Card size="small" onClick={() => console.log(item.itemId)} style={{ width: '150px', backgroundColor: 'lightgreen', fontSize: "16px", color: 'black' }} 
                   extra={
-                    <EditOutlined style={{ fontSize: 'large'}} onClick={() => {console.log("edit", item.itemName); setEditModal(item)}}/>}
+                    <EditOutlined style={{ fontSize: 'large'}} onClick={() => {console.log("edit", item.itemId); setEditModal(item)}}/>}
                   >{item.itemName} </Card> :
-                  <Card size="small" onClick={() => console.log(item.itemName)} style={{width: '150px', backgroundColor: 'lightgray', fontSize: "16px", color: 'gray'}} 
+                  <Card size="small" onClick={() => console.log(item.itemId)} style={{width: '150px', backgroundColor: 'lightgray', fontSize: "16px", color: 'gray'}} 
                   extra={
-                    <EditOutlined style={{ fontSize: 'large'}} onClick={() => {console.log("edit", item.itemName); setEditModal(item)}}/>}
+                    <EditOutlined style={{ fontSize: 'large'}} onClick={() => {console.log("edit", item.itemId); setEditModal(item)}}/>}
                   >{item.itemName} </Card>
                 );
               })}
+              <Card size="small" onClick={() => setAddModal(true)} style={{ width: '150px', backgroundColor: '#e0e0e0', fontSize: "16px", color: 'black',display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
+                <PlusCircleOutlined style={{ fontSize: '28px'}}/>
+              </Card>
+              
+              <Modal title="修改品項" centered open={editModal!==""} onOk={() => setEditModal("")} onCancel={() => setEditModal("")}>
+                <Input size="large" addonBefore="品名" placeholder={editModal.itemName} />
+                <Radio.Group onChange={onChangeRadio} defaultValue={i}>
+                  {tabs.map((tab, i0) => {
+                    return (<Radio.Button value={i0}>{tab}</Radio.Button>);
+                  })}
+                </Radio.Group>
+                <InputNumber size="large" addonBefore="價格" placeholder={editModal.price} />
+                {editModal.status === 'serving' ? 
+                    <Button size="large" >供應中</Button> :
+                    <Button size="large" >售完</Button>
+                }
+                <Button size="large" danger >刪除品項</Button>
+              </Modal>
+
+              <Modal title="新增品項" centered open={addModal} onOk={() => setAddModal(false)} onCancel={() => setAddModal(false)}>
+                <Input size="large" addonBefore="品名" />
+                <Input disabled size="large" addonBefore="類別" placeholder={tabs[i]} />
+                <InputNumber size="large" addonBefore="價格" />
+                <Button size="large" >供應中</Button>
+              </Modal>
             </div>
           };
         })}
         size="large"
       />
-      <Modal title="修改品項" centered open={editModal!==""} onOk={() => setEditModal("")} onCancel={() => setEditModal("")}>
-        <Input size="large" addonBefore="品名" placeholder={editModal.itemName} />
-        <Radio.Group onChange={onChangeRadio} >
-          {tabs.map((tab, i) => {
-            return (<Radio.Button value={i}>{tab}</Radio.Button>);
-          })}
-        </Radio.Group>
-        <InputNumber size="large"addonBefore="價格" placeholder={editModal.price} />
-        <Button size="large" >供應中</Button>
-        <Button size="large" danger >刪除品項</Button>
-      </Modal>
+
     </div>
 
   );
