ef91f6971333966c142482080ec25c2c4543e9f5
mchen127 <mchen127.p@gmail.com>
2023-05-29 12:08:04 +0800

    db v3

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBdMU3LuR9Tt_OVmQsYN8KDWhzGwxKJhVM",
-    authDomain: "shijiou-rmmps-is3.firebaseapp.com",
-    projectId: "shijiou-rmmps-is3",
-    storageBucket: "shijiou-rmmps-is3.appspot.com",
-    messagingSenderId: "914782855839",
-    appId: "1:914782855839:web:78f7dfac169080cb2838aa",
-    measurementId: "G-RY1X8DR5XV"
-  };
+  apiKey: "AIzaSyBdMU3LuR9Tt_OVmQsYN8KDWhzGwxKJhVM",
+  authDomain: "shijiou-rmmps-is3.firebaseapp.com",
+  projectId: "shijiou-rmmps-is3",
+  storageBucket: "shijiou-rmmps-is3.appspot.com",
+  messagingSenderId: "914782855839",
+  appId: "1:914782855839:web:78f7dfac169080cb2838aa",
+  measurementId: "G-RY1X8DR5XV"
+};
   
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
